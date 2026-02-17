f01409458e8ed1184fb3225493e3ea9e3a860e4d
Joemol Joy <joemoljoy.a@gmail.com>
2026-02-17 16:53:15 -0500

    Craete new component task inside tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,42 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-//@Input() selectedUserName: string | undefined;
-@Input() selectedUserName?: string;
+  //@Input() selectedUserName: string | undefined;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) selectedUserName!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn the basics of Angular',
+      dueDate: '2026-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Build a Project',
+      summary: 'Create a sample project using Angular',
+      dueDate: '2026-01-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Deploy Application',
+      summary: 'Deploy the application to a production environment',
+      dueDate: '2026-02-28'
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
